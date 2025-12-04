361e0ad0b01cd3bb194c3eb306554c03854d18f8
Lukasz6855 <lukasz6855@op.pl>
2025-12-04 20:38:09 +0100

    Dodanie plików sitemap i robots.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://pomocnikprezentowy.pl';
+  const baseUrl = 'https://prezentyai.pl';
   
   return [
     {
